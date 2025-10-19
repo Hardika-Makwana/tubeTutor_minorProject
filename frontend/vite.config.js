885d31c907a2906feb34df40c8d0c6f4cba35f03
Hardika-Makwana <166051037+Hardika-Makwana@users.noreply.github.com>
2025-10-19 19:20:46 +0530

    updated frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/videos': 'http://127.0.0.1:8000',
-      '/video': 'http://127.0.0.1:8000',
-      '/upload_video': 'http://127.0.0.1:8000',
-      '/uploaded_videos': 'http://127.0.0.1:8000',
-    },
-  },
 })
+
 
